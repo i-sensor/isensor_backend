0700612d95c44583c2feca2eabcf13854141458d
Anatoliy Gargash <kabantolyan@gmail.com>
2022-04-01 16:50:22 +0300

    deploy fixes

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser')
 const sensorRouter = require('./sensor/sensor.route')
 
 const app = express()
-const PORT = process.env.APP_PORT ?? 8000
+const PORT = process.env.PORT || 8000
 
 app.use(bodyParser.json())
 app.use(sensorRouter)
