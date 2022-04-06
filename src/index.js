f9314cc57f5c726d2fbccf915b88ba9b1785fbd8
Anatoliy Gargash <kabantolyan@gmail.com>
2022-04-06 20:24:38 +0300

    use cors

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const sensorRouter = require('./sensor/sensor.route')
+const cors = require('cors')
 
 const app = express()
 const PORT = process.env.PORT || 8000
 
+app.use(cors())
 app.use(bodyParser.json())
 app.use(sensorRouter)
 app.use((req, res, next) => {
